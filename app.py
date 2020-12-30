758bd62009d7073678730cbee9eb1f1219f701e4
Duayt <poom_tanawat@hotmail.com>
2020-12-30 17:30:21 +0700

    reply bot

--- app.py
+++ app.py
@@ -35,10 +35,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text.startswith('#ผวน'):
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(
+    event.reply_token,
+    TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
