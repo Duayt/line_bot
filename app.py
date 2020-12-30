7e8ef46caadc06f039550848a3f7c3a10feee652
Tanawat Chiewhawan <poom_tanawat@hotmail.com>
2020-12-30 21:48:46 +0700

    print signature

--- app.py
+++ app.py
@@ -18,11 +18,12 @@ handler = WebhookHandler(CHANNEL_SECRET)
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
+    print(signature)
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
 
+    app.logger.info("Request body: " + body)
+    
     # handle webhook body
     try:
         handler.handle(body, signature)
